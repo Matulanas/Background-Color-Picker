6347ccd690a616bf130d92576eb4e5f79735ee46
Matulanas <61939694+Matulanas@users.noreply.github.com>
2021-09-07 22:01:19 +0300

    Add existing file

--- src/App.js
+++ src/App.js
@@ -1,25 +1,46 @@
-import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
+import data from '../src/data/crayola.json';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+    const [hex, setHex] = useState('#6E5160')
+    const [name, setName] = useState('Eggplant')
+    const [rgb, setRgb] = useState('(110, 81, 96)')
+
+    const colorHandler = (e) => {
+        setHex(e.hex)
+        setName(e.name)
+        setRgb(e.rgb)
+    }
+
+    return (
+        <div className="App" style={{backgroundColor: hex}}>
+            <header className="App-header">
+                Background Color Picker
+            </header>
+
+            <div className="color-palette">  
+                { 
+                    data.map(data => 
+                        <button 
+                            className="btn" 
+                            value={data} 
+                            onClick={() => colorHandler(data)} 
+                            style={{backgroundColor: data.hex}}
+                            title={data.name}>
+                        </button>
+                    )
+                }
+                <div className="color-info">
+                    <p>Selected color:</p> 
+                    <span>Name: <b>{name}</b></span>
+                    <span>HEX: <b>{hex}</b></span>
+                    <span>RGB: <b>{rgb}</b></span>
+                </div>
+            </div>
+        </div>
+    );
 }
 
 export default App;
